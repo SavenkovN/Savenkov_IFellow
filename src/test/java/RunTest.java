5d5d072737a8436fc78805743ee9b4c9be3eeefe
Savenkov Nikita <nekk.sav@mail.ru>
2024-09-10 01:50:20 +0400

    Первый тест в ветке hotfix

---
+++ src/test/java/RunTest.java
@@ -0,0 +1,40 @@
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.*;
+
+public class RunTest {
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("beforeAll");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("afterAll");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("afterEach");
+    }
+
+    @Test
+    @Tag("Позитивный сценарий")
+    @Owner("Никита Савенков")
+    public void test1(){
+        System.out.println("1");
+        String s = "AW";
+        Assertions.assertEquals("AW",s);
+
+    }
+    @Test
+    @Tag("Негативный сценарий")
+    @Owner("Никита Савенков")
+    public void test2(){
+        System.out.println("2");
+        String s = "Asad@W";
+        Assertions.assertEquals("AW",s);
+
+    }
+
+
+}
